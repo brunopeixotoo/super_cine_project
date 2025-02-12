1cbed176c7db5859725e77559831d99e7c0f0d74
Bruno Rodrigues <brunorodripeix@gmail.com>
2025-02-12 10:17:43 -0300

    Update de error

--- error.vue
+++ error.vue
@@ -2,7 +2,7 @@
   <div class="flex gap-1 min-h-screen bg-gray-900 items-center text-3xl">
     <div class="flex flex-col gap-4 m-5">
       <span class="text-white">
-        Ops! Não foi possível encontrar a página.
+        Ops! Não foi possível encontrar essa página.
       </span>
 
       <MovButton
